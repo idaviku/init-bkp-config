dadc53a7673ea1a5adf55e9f1e6ece3c63e3922e
davik <xnk.davik@gmail.com>
2024-10-26 23:15:11 -0500

    add script to create a bkp of the previous version of configuracion files

---
+++ scripts/bkp4dummies.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# Nombre del script: bkp4dummies.sh 
+# Descripción: Script para mantener un punto anterior al actual de algunos
+# archivos de configuracion, se usa en conjunto con vim para facilitar la
+# gestion de las copias.
+# Autor: @idaviku
+# Fecha de creación: 2024-10-26
+# Última modificación: 2024-10-26
+# -----------------------------------------------------------------------------
+
+
+files=("$HOME/.archivo1.conf" "$HOME/.archivo2.conf" "$HOME/.archivo3.conf")
+backup_dir="$HOME/"
+
+for file in "${files[@]}"; do
+  if [ -f "$file" ]; then
+    backup_file="${file}.bak"
+    temp_file="${file}.tmp"
+    cp "$file" "$temp_file"
+    if [ -f "$backup_file" ]; then
+      cmp "$file" "$backup_file"
+      if cmp -s "$temp_file" "$backup_file"; then
+        rm "$temp_file"
+        continue
+      fi
+    fi
+
+    cp "$temp_file" "$backup_file"
+    echo "Respaldo creado: $backup_file"
+
+    rm "$temp_file"
+  fi
+done
