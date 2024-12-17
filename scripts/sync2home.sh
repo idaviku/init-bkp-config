47ca942a2bdb751d45929c5e8528454a7d877e28
Minos mbi <xnk.davik@gmail.com>
2024-12-16 23:52:43 -0500

    update scripts to use rsync or cp based on availability, and select config files depending on the os

--- scripts/sync2home.sh
+++ scripts/sync2home.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
-bkp_path=$(dirname $(dirname "$(realpath "$0")"))
-rsync -azP $bkp_path/.toprc $HOME/.toprc
-rsync -azP $bkp_path/.vimrc $HOME/.vimrc
-rsync -azP $bkp_path/.zshrc $HOME/.zshrc
-rsync -azP $bkp_path/.tmux.conf $HOME/.tmux.conf
+# -----------------------------------------------------------------------------
+# Nombre del script: sync2home.sh 
+# Descripción: Script para sincronizar los archivos de configuracion del
+# repositorio a el HOME
+# Autor: @idaviku
+# Fecha de creación: 2024-10-26
+# Última modificación: 15/12/2024 domingo
+# -----------------------------------------------------------------------------
+
+bkp_path=$(dirname $(dirname "$(realpath "$0")"))  
+files=(".toprc" ".vimrc" ".zshrc" ".tmux.conf" "_vimrc")
+
+if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+  files=("_vimrc")
+fi
+
+if command -v rsync &> /dev/null; then
+  for file in "${files[@]}"; do
+    src="$bkp_path/$file"
+    dest="$HOME/$file"
+    rsync -azP "$src" "$dest"
+  done
+else
+  for file in "${files[@]}"; do
+    src="$bkp_path/$file"
+    dest="$HOME/$file"
+    cp "$src" "$dest"
+  done
+fi
--- scripts/sync2repo.sh
+++ scripts/sync2repo.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
-bkp_path=$(dirname $(dirname "$(realpath "$0")"))
-rsync -azP $HOME/.config/procps/toprc $bkp_path/.toprc
-rsync -azP $HOME/.vimrc $bkp_path
-rsync -azP $HOME/.zshrc $bkp_path
-rsync -azP $HOME/.tmux.conf $bkp_path
+# -----------------------------------------------------------------------------
+# Nombre del script: sync2repo.sh 
+# Descripción: Script para sincronizar los archivos de configuracion del
+# repositorio a el REPOSITORIO
+# Autor: @idaviku
+# Fecha de creación: 2024-10-26
+# Última modificación: 15/12/2024 domingo
+# -----------------------------------------------------------------------------
+
+bkp_path=$(dirname $(dirname "$(realpath "$0")"))  
+files=(".toprc" ".vimrc" ".zshrc" ".tmux.conf" "_vimrc")
+
+if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+  files=("_vimrc")
+fi
+
+if command -v rsync &> /dev/null; then
+  for file in "${files[@]}"; do
+    src="$bkp_path/$file"
+    dest="$HOME/$file"
+    rsync -azP "$dest" "$src"
+  done
+else
+  for file in "${files[@]}"; do
+    src="$bkp_path/$file"
+    dest="$HOME/$file"
+    cp "$dest" "$src"
+  done
+fi
